cf4b9bcf6c02868c5f044836e56b61770ffad984
Kevin Strauß <kevin_strauss@gmx.de>
2018-10-18 14:33:18 +1100

    add ability to deploy and pullback model via REST calls

--- lstm-forecast-api/src/main.py
+++ lstm-forecast-api/src/main.py
@@ -1,6 +1,8 @@
 import logging
 import view.app as app_rest
-from controller.lstm import LSTMSeries
+#from controller.lstm import LSTMSeries
+import argparse
+
 #import view.kafkastreams.app as app_kafkastreams
 
 # Configure logger
@@ -9,14 +11,17 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s"
     )
 
+
+
 # main method
 def main():
-    
-    app_rest.start(appliances = ['FGE','HTE','TVE'])
+
+    app_rest.start()
     
     
 # start main method
 if __name__ == '__main__':
+
     main()
 
 
--- lstm-forecast-api/src/view/app.py
+++ lstm-forecast-api/src/view/app.py
@@ -8,22 +8,50 @@ import logging
 # set global vars
 app = Flask(__name__)
 
-def start(appliances):
-    """ Starts a REST server with Cognitive Tagging API """
-    # debug
-    logging.info("--- Start LSTM Forecast API ---")
-    logging.info("Load model ...")
 
-    global lstm_models
-    lstm_models = {}
-
-    for appliance in appliances:
-        logging.info("Initialize model for appliance: " + appliance)
-        lstm_models[appliance] = LSTMSeries(appliance + '_train_values',appliance+'_model_1',appliance+'_scaler')
-        lstm_models[appliance].init_state(appliance+'_train_normalized')
-    
-    logging.info("Start web service ...")
+def start():
+    """ Starts a REST server """
     app.run(host='0.0.0.0')
+
+
+
+
+@app.route('/lstmforecast/deploy-model', methods=['POST'])
+def deploymodel():
+    """ Starts a REST server with Cognitive Tagging API """
+
+    appliances = request.json.get('appliances')
+    print("APPLIANCES  :    " + str(appliances))
+
+    if len(appliances) != 0 :
+
+        # debug
+        logging.info("--- Start LSTM Forecast API ---")
+        logging.info("Load model ...")
+
+        global lstm_models
+        lstm_models = {}
+
+        for appliance in appliances:
+            logging.info("Initialize model for appliance: " + appliance)
+            lstm_models[appliance] = LSTMSeries(appliance + '_train_values',appliance+'_model_1',appliance+'_scaler')
+            lstm_models[appliance].init_state(appliance+'_train_normalized')
+        
+        logging.info("Start web service ...")
+    
+    else:
+        print("Please provide some appliance for which models shall be started")
+
+    return jsonify({'currently-deployed-models': str(appliances)}), 200
+
+
+
+@app.route('/lstmforecast/pullback-model', methods=['POST'])
+def pullbackmodel():
+    global lstm_models 
+    lstm_models = {}
+    return jsonify({'currently-deployed-models': str(list(lstm_models.keys()))}), 200
+
 
 
 """global lstm
@@ -47,7 +75,6 @@ def internal_error(error):
 @app.errorhandler(400)
 def internal_error_400(error):
     return jsonify({'error': "Die Anfrage wurde syntaktisch falsch erstellt."}), 400
-
 
 
 @app.route('/lstmforecast/<string:appliance>/predict', methods=['POST'])
